231bbda0e13739fb8ce189c4bc5fd5a959c3a4b3
Alex <demsign@gmail.com>
2020-10-08 19:05:48 +0200

    Add restrictions and validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
   has_many :items
+  validate :name, uniqueness: true
+  validate :quantity, numericality: { only_integer: true }
 end
--- db/migrate/20201008165758_create_products.rb
+++ db/migrate/20201008165758_create_products.rb
@@ -1,8 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
     create_table :products do |t|
-      t.string :name
-      t.integer :quantity
+      t.string :name, null: false
+      t.integer :quantity, default: 0
 
       t.timestamps
     end
