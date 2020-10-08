21e6ae3f4f62c15ceed97f943d3e4c7dd18c07b9
Alexei Maridashvili <alexei.maridashvili@gmail.com>
2020-10-08 16:01:15 +0200

    Find booking contract for the given period

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  has_many :bookings
+
+  def reserved?(from, till)
+    from = DateTime.parse(from])
+    till = DateTime.parse(till)
+    bookings.where("(rental_start <= ? AND rental_end > ?) OR (rental_start BETWEEN ? AND ?)", from, from, from, till).exists?
+  end
 end
