044b9f866ac383c21c0d60599b5e32b75b408df7
Alex <demsign@gmail.com>
2020-10-09 08:07:49 +0200

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.find_or_create_by(name: 'Nikon D700', quantity: 10)
+5.times do |sku|
+  Item.find_or_create_by(product: product, sku: "nsku_#{sku}")
+end
