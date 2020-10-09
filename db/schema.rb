e09dd556a87156bc364b228fc2b5dbb1f0b448df
Alex <demsign@gmail.com>
2020-10-09 08:00:20 +0200

    Add SKU to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_171029) do
+ActiveRecord::Schema.define(version: 2020_10_09_055751) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_10_08_171029) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "product_id"
+    t.string "sku"
     t.index ["product_id"], name: "index_items_on_product_id"
   end
 
