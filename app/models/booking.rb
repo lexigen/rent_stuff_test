44f9a4e04d2c3f9b8ec5e7c65a48791086604001
Alex <demsign@gmail.com>
2020-10-09 13:43:33 +0200

    Add validations to models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :item
 
   validates :item_id, uniqueness: { scope: [:user_id, :rental_start, :rental_end] }
+  validates :user, presence: true
+  validates :rental_start, presence: true
+  validates :rental_end, presence: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   has_many :bookings
   belongs_to :product
 
+  validates :product, presence: true
+
   def reserved?(from, till)
     bookings.where("(rental_start <= ? AND rental_end > ?) OR (rental_start BETWEEN ? AND ?)", from, from, from, till).exists?
   end
